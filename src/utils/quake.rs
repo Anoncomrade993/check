0fda4ea1595b11db5d7b93bba612ab4d7ddb363b
Anoncomrade993 <ifeanyiotah001@gmail.com>
2024-03-02 11:11:47 +0100

    update quake.rs

--- src/utils/quake.rs
+++ src/utils/quake.rs
@@ -1,7 +1,14 @@
 use image::GenericImageView;
 use image::Pixel;
 use pixelate::algorithms::lsb::LSB;
-#[derive(Debug)]
+use base64::{Engine as _,engine::{self,general_purpose::STANDARD as st}};
+use serde::{ json,json::{Json,Value}, Deserialize, Serialize },
+use std::error::Error; 
+use std::fs::File;
+use std::io::BufRead;
+
+
+#[derive(Debug, Deserialize, Serialize)]
 struct Data<'a>{
     channel: u8,
     pixels: Vec<u8>,
@@ -22,7 +29,22 @@ fn read_image(path: &str) -> Result<Vec<u8>, image::ImageError> {
     Ok(pixel_data)
 }
 
+fn read_json(path:&str) -> Result<Json<Data>,Error>{
+  let mut store = String::new();
+  let file = File::open(path)?;
+  let mut buffer = BufRead::new(file)?;
+  buffer.read_line(&mut store)?
+  let res :Json<Data> = serde_json::from_str(&store)
+  Ok(res)
+}
 
+fn base_encode(s:&str)-> &'static str{
+  st.encode(s)
+}
+
+fn base_decode(s:&str)-> &'static str{
+  st.decode(s)
+}
 
 fn binary(text: String) -> String {
     text.chars().map(|c| format!("{:08b}", c as u8)).collect::<Vec<String>>().join("")
@@ -41,20 +63,22 @@ fn binary(text: String) -> String {
 }
 **/
 fn data_encode(strng: &str) -> String {
-    let bin = binary(strng.to_string());
-   bin // base_encode(&bin)
+    let base = base_encode(&strng);
+    binary(base.to_string())
+}
+
+fn data_decode(strng: str) -> &'static str {
+    let bin = unbinary(strng.to_string())
+    base_encode(&bin)
 }
 
 pub fn handler() -> Result<Vec<u8>, &'static str> {
    let data = "hello ness";
     let f_path = "./src/assets/img.jpg";
     let mut channel = 2u8;
-   // let pixels = read_image(f_path).map_err(|_| "error reading image")?;
-    //let message = &jf.message;
     let message = data_encode(data);
     let mut pixels = read_image(f_path).map_err(|_| "error reading image")?;
 
-    // Assuming LSB::encode modifies the pixels and returns a Result<(), ErrorType>
      let lsb_enc = LSB::encode(&mut pixels, &message,&mut channel).unwrap();
 
     let d = Data::new(channel, pixels, &message);
